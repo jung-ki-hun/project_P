5fba2cee7b10de32b23bd83a4a7cec8bf7e5fcc2
jungkihun <khkh0130@gmail.com>
2021-07-28 01:15:38 +0900

    jkh 0004_1

---
+++ javascript/k_number.js
@@ -0,0 +1,15 @@
+/***
+ * k번째수
+ * 
+ * https://programmers.co.kr/learn/courses/30/lessons/42748?language=javascript
+ */
+
+ var array_cut =(arr,i,j,k)=>{
+    let arr_c = arr.slice(i,j);
+    arr_c.sort()
+    return arr_c[k];
+}
+function solution(array, commands) {
+    var answer = ar;
+    return answer;
+}
