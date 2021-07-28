5fdc01dea693d7708ab0b34d4379ce1405f12c00
jungkihun <khkh0130@gmail.com>
2021-07-29 05:37:17 +0900

    jkh 0004_2

---
+++ cpp/k_number.cpp
@@ -0,0 +1,16 @@
+/***
+ * k번째수
+ * 
+ * https://programmers.co.kr/learn/courses/30/lessons/42748?language=javascript
+ */
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+
+    return answer;
+}
